9eac995d16c686e0b2411ab0bba93a295ce67dc4
Stephen Johnson <stevejohnson3293@gmail.com>
2025-05-20 21:50:20 -0500

    Sprint 5 spots project update 1.2

--- scripts/index.js
+++ scripts/index.js
@@ -46,22 +46,30 @@ const linkInput = newPostModal.querySelector("#card-image-input");
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+function openModal(modal) {
+  modal.classlist.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classlist.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostCloseBtn);
 });
 
 function handleEditProfileSubmit(evt) {
